9c628dd4eb98786751da57745ad2cfc046141d05
Manish Trivedi <manishtrivedi@Manishs-iMac.local>
2019-07-18 18:00:03 +0530

    Initial Commit 2

--- StringExtension.swift
+++ StringExtension.swift
@@ -15,4 +15,11 @@ extension String {
         let word = component.filter{!$0.isEmpty}
         return word.count
     }
+    
+    public var charCount: Int {
+        let characterSet = CharacterSet.whitespacesAndNewlines
+        let component = self.components(separatedBy: characterSet)
+        let word = component.flatMap({ $0.map({ $0 })})
+        return word.count
+    }
 }
